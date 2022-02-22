d0ae414d737f7116c6d9ea8152159276c0ee0bd5
priyankarai11 <53466995+priyankarai11@users.noreply.github.com>
2022-02-22 08:24:05 +0530

    updated app.js

--- App.js
+++ App.js
@@ -1,39 +1,38 @@
 import React from 'react';
-import { useRef ,useState} from 'react';
+import { useRef, useState } from 'react';
 import ToDoList from './ToDoList';
 import { uid } from 'uid';
-import List from './ToDoList'
+import List from './ToDoList';
 
 function App() {
-  const [toDos, setToDos]=useState([{name:"Todo1", id: "1",completed:false},{name:"Todo2", id:"2",completed:false},{name:"Todo3", id:"3",completed:false}])
-  const conRef=useRef();
-  const submit=()=>
-  {
-    let todo={name:conRef.current.value, id:uid(), completed:false};
-    let newTodos=[...toDos,todo]
-    setToDos(
-      newTodos
-    )
-  }
-  
- const onChangeBox = item => {
-    this.setState(({ todos }) => ({
-      todos: todos.map(el =>
-        el.id === item.id ? { ...el, completed: !el.completed } : el
-      )
-    }));
+  const [toDos, setToDos] = useState([
+    { name: 'Todo1', id: '1', completed: false },
+    { name: 'Todo2', id: '2', completed: false },
+    { name: 'Todo3', id: '3', completed: false },
+  ]);
+  const conRef = useRef();
+  const submit = () => {
+    let todo = { name: conRef.current.value, id: uid(), completed: false };
+    let newTodos = [...toDos, todo];
+    setToDos(newTodos);
+  };
+
+  const onChangeBox = (e, item) => {
+    const newCheckboxValue = e.target.checked;
+
+    const newTodo = toDos.map((el) =>
+      el.id === item.id ? { ...el, completed: newCheckboxValue } : el
+    );
+
+    setToDos(newTodo);
   };
 
   return (
     <>
-    <input type="text" ref={conRef}  />
-    <button onClick={submit} >Add</button>
+      <input type="text" ref={conRef} />
+      <button onClick={submit}>Add</button>
 
-     <ToDoList
-          list={toDos}
-          onChangeBox={onChangeBox}
-        />
-
+      <ToDoList list={toDos} onChangeBox={onChangeBox} />
     </>
   );
 }
