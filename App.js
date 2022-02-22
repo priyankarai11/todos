1d5a03e129d04de7ba57597da5562f5a9ab47feb
priyankarai11 <53466995+priyankarai11@users.noreply.github.com>
2022-02-22 17:25:22 +0530

    added localstorage function to app.js

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRef, useState } from "react";
 import ToDoList from "./ToDoList";
 import { uid } from "uid";
 
+const getLocalItems = () => {
+  let list = localStorage.getItem("lists");
+  console.log(list);
+  if (list) {
+    return JSON.parse(localStorage.getItem("lists"));
+  } else {
+    return [];
+  }
+};
 
+const items = [
+  { name: "Todo1", id: "1", completed: false },
+  { name: "Todo2", id: "2", completed: false },
+  { name: "Todo3", id: "3", completed: false },
+];
 
 function App() {
-  const [toDos, setToDos] = useState([
-    { name: "Todo1", id: "1", completed: false },
-    { name: "Todo2", id: "2", completed: false },
-    { name: "Todo3", id: "3", completed: false },
-  ]);
-
+  const [toDos, setToDos] = useState(getLocalItems);
 
   const conRef = useRef();
+
   const handleSubmit = () => {
     let todo = { name: conRef.current.value, id: uid(), completed: false };
     let newTodos = [...toDos, todo];
     setToDos(newTodos);
     conRef.current.value = null;
   };
-
-
 
   const onChangeBox = (e, item) => {
     const newCheckboxValue = e.target.checked;
@@ -35,33 +43,24 @@ function App() {
     setToDos(newTodo);
   };
 
-
-
   const todoDel = (id) => {
+    console.log(id);
     const newTodos = [...toDos];
     newTodos.splice(id, 1);
     setToDos(newTodos);
   };
 
-
-
-  const handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.ref,
-    });
-  };
-
-
-
   const clear = () => {
     setToDos([]);
   };
 
-
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(toDos));
+  }, [toDos]);
 
   return (
     <span>
-      <input type="text" ref={conRef} name="name" onChange={handleChange} />
+      <input type="text" ref={conRef} name="name" />
       <button onClick={handleSubmit}>Add</button>
 
       <ToDoList
@@ -70,11 +69,8 @@ function App() {
         todoDel={todoDel}
         clear={clear}
       />
-      
     </span>
   );
 }
-
-
 
 export default App;
